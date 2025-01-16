4c1dce9f05ef20b3936b60118e99d7d472813054
Faham <towhidurfaham@gmail.com>
2025-01-16 20:21:28 +0600

    Login Button(Modal)

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <style>
     * {
       font-family: "Poppins", serif;
@@ -46,11 +47,45 @@
           </li>
         </ul>
         <div class="d-flex">
-          <button class="btn btn-outline-success shadow-none" type="submit">Search</button>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+            Login
+          </button>
         </div>
       </div>
     </div>
   </nav>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <form>
+          <div class="modal-header">
+            <h5 class="modal-title d-flex align-items-center">
+              <i class="bi bi-person-circle fs-3 me-2"></i> User Login
+            </h5>
+            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="mb-3">
+              <label class="form-label">Email address</label>
+              <input type="email" class="form-control" id="form-control shadow-none">
+            </div>
+            <div class="mb-4">
+              <label class="form-label">Password</label>
+              <input type="password" class="form-control" id="form-control shadow-none">
+            </div>
+            <div class="d-flex align-items-center justify-content-between mb-2">
+              <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+              <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forget Password</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 
 
 
